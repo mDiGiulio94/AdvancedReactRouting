8371075097cb3880777ff487cc225228b96fe356
Marco Di Giulio <m.digiulio@jdk.it>
2026-05-13 20:46:31 +0200

    useFetcher

--- frontend/src/components/NewsletterSignup.js.js
+++ frontend/src/components/NewsletterSignup.js.js
@@ -1,15 +1,32 @@
-import classes from './NewsletterSignup.module.css';
+import classes from "./NewsletterSignup.module.css";
+import { useFetcher } from "react-router-dom";
+import { useEffect } from "react";
 
 function NewsletterSignup() {
+  // viene usato quando si vuole far partire un azione o un loader senza però navigare alla pagina dove l'azione o il loader si trovano
+  const fetcher = useFetcher();
+  // fetcher ha molte proprietà non solo la funzione sopra ad esempio si possono accedere a tuttii data con la proprietà data ad esempio
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert("Submitted");
+    }
+  }, [data, state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
